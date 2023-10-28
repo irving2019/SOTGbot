ce74aec6ca7e38c6edcc66150cabf0a6ef3adca3
Vladimir Pilipenko <52373540+irving2019@users.noreply.github.com>
2023-10-28 14:33:42 +0500

    Person.py
    Персонаж и его характеристики

---
+++ Person.py
@@ -0,0 +1,13 @@
+class Person:
+    def __init__(self, power, hp, energy, armor, mood, group, klan, radioactive, blood, hunger, thirst):
+        self.power = power  # Переносимый вес
+        self.hp = hp  # Здоровье
+        self.energy = energy  # Усталость
+        self.armor = armor  # Броня
+        self.mood = mood  # Настроение
+        self.group = group  # Группировка
+        self.klan = klan  # Клан
+        self.radioactive = radioactive  # Заражение радиацией
+        self.blood = blood  # кровотечение
+        self.hunger = hunger  # голод
+        self.thirst = thirst  # обезвоживание
