24f95e08f5f143016eae7f549e4b01820b68e98a
Vladimir Pilipenko <52373540+irving2019@users.noreply.github.com>
2023-10-28 14:32:06 +0500

    Groups.py

---
+++ Groups.py
@@ -0,0 +1,33 @@
+from Locations import *
+from Person import *
+from Armors import *
+from Guns import *
+
+
+class Groups:
+    def __init__(self, name, friends, neutrals, enemies, outfit, location, armor, guns):
+        self.name = name
+        self.friends = friends
+        self.neutrals = neutrals
+        self.enemies = enemies
+        self.outfit = outfit
+        self.location = location
+        self.guns = guns
+
+
+# Группировки:
+Single_Group = "Вольные сталкеры"
+Military_Group = "Военные"
+Freedom_Group = "Свобода"
+Duty_Group = "Долг"
+Monolit_Group = "Монолит"
+Clear_sky_Group = "Чистое небо"
+Sin_Group = "Грех"
+Bandit_Group = "Бандиты"
+Renegade_Group = "Ренегаты"
+Awarenes_Group = "Осознание"
+Scientist_Group = "Учёные"
+Mercenaries_Group = "Наёмники"
+Zombie_Group = "Зомби"
+Mutant_Group = "Мутанты"
+Merchant_Group = "Торговцы"
